90d45d1b5d4d62d9382ded4d5649be4937f51a23
jeevaathecoder <jeevaathecoder@gmail.com>
2024-04-08 11:40:12 +0530

    restaurant Backend donee

---
+++ Online Food Ordering System/Food Ordering Application/src/test/java/com/zigzagcoding/FoodOrderingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zigzagcoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodOrderingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
