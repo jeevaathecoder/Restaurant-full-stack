12b9715bfe822a35cdcba73fb8b12df933e1dd5d
Jeeva S <109788234+jeevaathecoder@users.noreply.github.com>
2024-04-08 11:37:57 +0530

    Delete HotelBookingApplication directory

--- HotelBookingApplication/HotelBooking-Api/src/main/java/com/hotel/repository/RoomRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hotel.repository;
-
-import com.hotel.model.Room;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoomRepository extends JpaRepository<Room,Long> {
-}
